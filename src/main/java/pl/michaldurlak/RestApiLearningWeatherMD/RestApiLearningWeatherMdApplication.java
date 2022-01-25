1066f59510ebb1282ca7622ddeebc2c89e5be007
Michał Durlak <durlak.m@wp.pl>
2022-01-25 23:03:02 +0100

    Initial commit

---
+++ src/main/java/pl/michaldurlak/RestApiLearningWeatherMD/RestApiLearningWeatherMdApplication.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.RestApiLearningWeatherMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiLearningWeatherMdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiLearningWeatherMdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/michaldurlak/RestApiLearningWeatherMD/RestApiLearningWeatherMdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.RestApiLearningWeatherMD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiLearningWeatherMdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
